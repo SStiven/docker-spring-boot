9d281436dc5315c39b16811648b15baed8da77cb
Stiven Villegas <sstiven@protonmail.com>
2021-08-19 17:36:08 -0500

    Agrega cross origin resource sharing

--- src/main/java/com/acme/dockerspring/AuthorController.java
+++ src/main/java/com/acme/dockerspring/AuthorController.java
@@ -1,12 +1,14 @@
 package com.acme.dockerspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthorController {
     @Autowired
     private AuthorRepository authorRepository;
